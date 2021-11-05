91b8f549b2eaeea7ba75dc34189018d611c7f519
Oliver Graf <you@example.com>
2021-11-05 13:02:01 +0000

    bla2

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,16 @@
-network_name = "alkanet"
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 3.0"
 
-project_id = var.project 
+    project_id   = var.project
+    network_name = "alkanet-vpc"
+    routing_mode = "GLOBAL"
 
-subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+    subnets = [
+      {
+        subnet_name   = "gaurav-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+      }
+    ]
+}
