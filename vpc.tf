5a9e56d8bc5a95e68b635131b8c7e490bbf83126
Oliver Graf <you@example.com>
2021-11-05 13:34:51 +0000

    bla23

--- vpc.tf
+++ vpc.tf
@@ -4,13 +4,12 @@ module "network" {
 
 network_name = "alkanet"
 project_id = var.project 
-routing_mode = "GLOBAL"
 
 subnets = [
   {
     subnet_name   = "gaurav-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
-  },
+  }
  ]
 }
