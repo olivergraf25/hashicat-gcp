a6da1d2791338e3c80da42175dc381350765576e
Oliver Graf <you@example.com>
2021-11-05 13:48:59 +0000

    bla6

--- vpc.tf
+++ vpc.tf
@@ -2,10 +2,10 @@ module "network" {
   source  = "app.terraform.io/alkasaiz/network/google"
   version = "3.4.0"
   # insert required variables here
-  network_name = "gaurav_subnet"
+  network_name = "gaurav-network"
   project_id = var.project
-
-subnets = [
+  
+  subnets = [
   {
     subnet_name   = "gaurav-subnet"
     subnet_ip     = "10.100.10.0/24"
