cce9165c17a0ae07daceca43a78db1dabf21bf79
Oliver Graf <you@example.com>
2021-11-05 14:14:50 +0000

    bla31

--- vpc.tf
+++ vpc.tf
@@ -3,12 +3,12 @@ module "network" {
 
   version = "3.4.0"
   # insert required variables here
-  network_name = "gaurav-network"
+  network_name = "alka-network"
   project_id = var.project
 
   subnets = [
   {
-    subnet_name   = "gaurav-subnet"
+    subnet_name   = "alka-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
